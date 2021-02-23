85f47d45cbba97605d6141143ece129f9172b885
Thevaa Chandereng <chandereng@wisc.edu>
2021-02-23 18:09:35 -0500

    Update merge.R

--- Data/merge.R
+++ Data/merge.R
@@ -3,7 +3,10 @@ temp = list.files(pattern = "CLBP*")
 
 df <- NULL
 for(i in 1:length(temp)){
-  df[[i]] <- readxl::read_excel(temp[i])
+  df[[i]] <- readxl::read_excel(temp[i], 
+                                col_types = c(rep("guess", 5), 
+                                              "numeric", "numeric", 
+                                              rep("guess", 5)))
 }
 
 
